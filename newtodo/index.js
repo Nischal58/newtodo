474054cc067dc3649ebaba382121306f5144300a
Nischal58 <neeschal100@gmail.com>
2022-05-02 13:43:56 +0545

    todo
    updated todo

--- newtodo/index.js
+++ newtodo/index.js
@@ -1,18 +1,17 @@
-
-let tasklist= [
+let tasklist = [
     {
-        id:11231,
-        username:'Nischal Maharjan',
-        email:'neeschal@gmail.com',
-        phone:'9868136666'
+        id: 11231,
+        username: 'Nischal Maharjan',
+        email: 'neeschal@gmail.com',
+        phone: '9868136666'
     }
-    
+
 
 ];
 
-const displayTask = ()=>{
+const displayTask = () => {
     document.querySelector('#tasklist').innerHTML = '';
-    tasklist.forEach((val,index)=>{
+    tasklist.forEach((val, index) => {
         document.querySelector('#tasklist').innerHTML += `
         <div class = "task-card">
         <div class = "task-title">${val.username}</div>
@@ -31,107 +30,106 @@ const displayTask = ()=>{
         `;
     });
 }
+
+
 displayTask();
-const addToArray=()=>{
-    const [username,email,phone]=[
+
+
+const addToArray = () => {
+    const [username, email, phone] = [
         document.querySelector('#addUsername').value,
         document.querySelector('#addEmail').value,
         document.querySelector('#addphone').value,
 
     ];
-    const id = tasklist[tasklist.length-1].id+1;
+    const id = tasklist[tasklist.length - 1].id + 1;
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var phoneformat = /(?:\+977[- ])?\d{2}-?\d{7,8}/
 
+    //Calling validation from a function
+    validation(id, username, email, phone, mailformat, phoneformat);
+    displayTask();
 
-    if([username,email].includes('')){
+}
+//Created the validation function
+const validation = (id, username, email, phone, mailformat, phoneformat) => {
+    if ([username, email].includes('')) {
         alert('Please fill both boxes');
-        
+
 
     }
-    else if(!email.match(mailformat))
-    {
+    else if (!email.match(mailformat)) {
         alert('Invalid Email');
-        
+
     }
 
-   else if(!phone.match(phoneformat)){
-         alert('Invalid Phone');
+    else if (!phone.match(phoneformat)) {
+        alert('Invalid Phone');
 
 
-   }
-   else{
-    tasklist.push({
-        id,username,email,phone
-    });
-    
+    }
+    else {
+        tasklist.push({
+            id, username, email, phone
+        });
 
-    document.querySelector('#addUsername').value = '';
-    document.querySelector('#addEmail').value = '';
-    document.querySelector('#addphone').value = '';
 
-    
-}
-   displayTask(); 
+        document.querySelector('#addUsername').value = '';
+        document.querySelector('#editID').value = '';
+        document.querySelector('#addEmail').value = '';
+        document.querySelector('#addphone').value = '';
 
- 
+    }
 }
 
-const deleteTask = id=>{
+const deleteTask = id => {
     console.log(id);
-    tasklist = tasklist.filter(val=>val.id!==id);
+    tasklist = tasklist.filter(val => val.id !== id);
     console.log(tasklist);
     displayTask();
 }
 
 
 
-
-
-const getEditData = (i)=>{
+const getEditData = (i) => {
     document.querySelector('#editID').value = tasklist[i].id;
-
-    document.querySelector('#editUsername').value = tasklist[i].username;
-    document.querySelector('#editEmail').value= tasklist[i].email;
-    document.querySelector('#editphone').value= tasklist[i].phone;
-
-    
-   
+    document.querySelector('#addUsername').value = tasklist[i].username;
+    document.querySelector('#addEmail').value = tasklist[i].email;
+    document.querySelector('#addphone').value = tasklist[i].phone;
 }
-const saveEditData=()=>{
-  let  [id,username,email,phone]=[
-    document.querySelector('#editID').value ,
 
-    document.querySelector('#editUsername').value,
-    document.querySelector('#editEmail').value,
-    document.querySelector('#editphone').value,
-  ];
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  var phoneformat = /(?:\+977[- ])?\d{2}-?\d{7,8}/
-    if([username,email].includes('')){
+const saveEditData = () => {
+    let [id, username, email, phone] = [
+        document.querySelector('#editID').value,
+        document.querySelector('#addUsername').value,
+        document.querySelector('#addEmail').value,
+        document.querySelector('#addphone').value,
+    ];
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var phoneformat = /(?:\+977[- ])?\d{2}-?\d{7,8}/;
+    if ([username, email].includes('')) {
         alert('Please fill both boxes');
 
     }
-    else if(!email.match(mailformat))
-    {
+    else if (!email.match(mailformat)) {
         alert('Invalid Email');
-        
+
     }
 
-   else if(!phone.match(phoneformat)){
-    alert('Invalid Phone');
+    else if (!phone.match(phoneformat)) {
+        alert('Invalid Phone');
+
+
+    }
+
+    const arrIndex = tasklist.findIndex(v => v.id == id);
+
+    tasklist[arrIndex].username = username;
+    tasklist[arrIndex].email = email;
+    tasklist[arrIndex].phone = phone;
+
+
+    displayTask();
 
 
 }
-
-else{
-  const arrIndex = tasklist.findIndex(v=>v.id==id);
-
-  tasklist[arrIndex].username = username;
-  tasklist[arrIndex].email = email;
-  tasklist[arrIndex].phone = phone;
- displayTask();
-}
-
-}
-
